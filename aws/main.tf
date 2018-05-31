c738b98fec52af30294baa091e988413a6e49734
Cocotton <julien.parent.trudeau@gmail.com>
2018-05-31 19:29:01 -0400

    Add security group for the elb

--- aws/main.tf
+++ aws/main.tf
@@ -12,7 +12,7 @@ data "aws_availability_zones" "all" {}
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-a4dc46db"
   instance_type   = "t2.micro"
-  security_groups = ["${aws_security_group.example.id}"]
+  security_groups = ["${aws_security_group.example_instance.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -25,7 +25,7 @@ resource "aws_launch_configuration" "example" {
   }
 }
 
-resource "aws_security_group" "example" {
+resource "aws_security_group" "example_instance" {
   name = "example-instance"
 
   ingress {
@@ -40,7 +40,18 @@ resource "aws_security_group" "example" {
   }
 }
 
-resource "aws_autoscaling_group" "example" {
+resource "aws_security_group" "example_elb" {
+  name = "example-elb"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_autoscaling_group" "example_instance" {
   launch_configuration = "${aws_launch_configuration.example.id}"
   availability_zones   = ["${data.aws_availability_zones.all.names}"]
 
@@ -57,6 +68,7 @@ resource "aws_autoscaling_group" "example" {
 resource "aws_elb" "example" {
   name               = "elb-example"
   availability_zones = ["${data.aws_availability_zones.all.names}"]
+  security_groups    = ["${aws_security_group.example_elb.id}"]
 
   listener {
     lb_port           = 80
