f9f8f47098687fe5e120f13b2bacbe63496264b0
Cocotton <julien.parent.trudeau@gmail.com>
2018-05-31 18:03:21 -0400

    Expose a sample index.html

--- aws/main.tf
+++ aws/main.tf
@@ -6,6 +6,12 @@ resource "aws_instance" "example" {
   ami           = "ami-a4dc46db"
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello baby" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
   tags {
     Name = "terraform-example"
   }
