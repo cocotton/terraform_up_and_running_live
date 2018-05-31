fc5d6d21f6da8a6984ccc69c968276344a942151
Cocotton <julien.parent.trudeau@gmail.com>
2018-05-31 19:34:42 -0400

    Link the elb to the asg

--- aws/main.tf
+++ aws/main.tf
@@ -49,11 +49,21 @@ resource "aws_security_group" "example_elb" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_autoscaling_group" "example_instance" {
   launch_configuration = "${aws_launch_configuration.example.id}"
   availability_zones   = ["${data.aws_availability_zones.all.names}"]
+
+  load_balancers    = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
 
   min_size = 2
   max_size = 10
@@ -75,5 +85,13 @@ resource "aws_elb" "example" {
     lb_protocol       = "http"
     instance_port     = "${var.server_port}"
     instance_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 30
+    target              = "HTTP:${var.server_port}/"
   }
 }
