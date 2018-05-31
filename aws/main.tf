ecd6bf58ee01a6ac78e23d99f4bfc610eb605ae0
Cocotton <julien.parent.trudeau@gmail.com>
2018-05-31 18:58:09 -0400

    Use an auto scaling group to deploy the instances

--- aws/main.tf
+++ aws/main.tf
@@ -7,10 +7,12 @@ variable "server_port" {
   default     = 8080
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-a4dc46db"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.example.id}"]
+data "aws_availability_zones" "all" {}
+
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-a4dc46db"
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.example.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -18,8 +20,8 @@ resource "aws_instance" "example" {
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
 
-  tags {
-    Name = "terraform-example"
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -32,8 +34,22 @@ resource "aws_security_group" "example" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-output "instance_public_ip" {
-  value = "${aws_instance.example.public_ip}"
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones   = ["${data.aws_availability_zones.all.names}"]
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "asg-example"
+    propagate_at_launch = true
+  }
 }
