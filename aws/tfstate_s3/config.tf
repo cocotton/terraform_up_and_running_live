f7aca22ebb48b22132652b17651fd6269e75d27b
Cocotton <julien.parent.trudeau@gmail.com>
2018-06-01 17:45:39 -0400

    Add terraform backend config manifest

---
+++ aws/tfstate_s3/config.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket  = "cocotton-terraform-up-and-running-state"
+    key     = "global/s3/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = "true"
+  }
+}
