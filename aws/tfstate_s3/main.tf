1c963fd3edb2cc8b69e0e42d7df3e76a543a2b05
Cocotton <julien.parent.trudeau@gmail.com>
2018-06-02 14:16:15 -0400

    Create a DynamoDB table to manage Terraform locks

--- aws/tfstate_s3/main.tf
+++ aws/tfstate_s3/main.tf
@@ -14,6 +14,14 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 }
 
-output "s3_bucket_arn" {
-  value = "${aws_s3_bucket.terraform_state.arn}"
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "terraform-state-lock"
+  hash_key       = "LockID"
+  read_capacity  = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
