a9941b27035c93554928eddd5986c31b7ab12c2d
Cocotton <julien.parent.trudeau@gmail.com>
2018-06-02 14:54:00 -0400

    Fix s3 key

--- aws/dev/services/terraform-up-and-running/config.tf
+++ aws/dev/services/terraform-up-and-running/config.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "cocotton-terraform-up-and-running-state"
-    key     = "/dev/services/terraform-up-and-running/terraform.tfstate"
+    key     = "dev/services/terraform-up-and-running/terraform.tfstate"
     region  = "us-east-1"
     encrypt = "true"
     dynamodb_table = "terraform-state-lock"
