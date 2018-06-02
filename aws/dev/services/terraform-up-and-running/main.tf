09d81025c36b9dd585a33437b471cf56695c4d49
Cocotton <julien.parent.trudeau@gmail.com>
2018-06-02 17:32:55 -0400

    Pass the rendered template to the alc

--- aws/dev/services/terraform-up-and-running/main.tf
+++ aws/dev/services/terraform-up-and-running/main.tf
@@ -29,7 +29,7 @@ resource "aws_launch_configuration" "example" {
   instance_type   = "t2.micro"
   security_groups = ["${aws_security_group.example_instance.id}"]
 
-  user_data = "FILLEMEUP"
+  user_data = "${data.template_file.user_data.rendered}"
 
   lifecycle {
     create_before_destroy = true
