e80e357b8e446c506d3c486dc4eed6cabca011b9
Cocotton <julien.parent.trudeau@gmail.com>
2018-06-02 16:00:13 -0400

    Fix database name

--- aws/dev/data-stores/mysql/main.tf
+++ aws/dev/data-stores/mysql/main.tf
@@ -6,7 +6,7 @@ resource "aws_db_instance" "example_database" {
   engine            = "mysql"
   allocated_storage = 10
   instance_class    = "db.t2.micro"
-  name              = "example-database"
+  name              = "exampleDatabase"
   username          = "admin"
   password          = "${var.database_password}"
 }
