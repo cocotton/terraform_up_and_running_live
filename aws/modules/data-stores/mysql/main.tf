ea48374be7d1a0293c86c7ee7578ce1e05460b87
Cocotton <julien.parent.trudeau@gmail.com>
2018-06-04 14:42:27 -0400

    Skip final snapshot

--- aws/modules/data-stores/mysql/main.tf
+++ aws/modules/data-stores/mysql/main.tf
@@ -3,10 +3,11 @@ provider "aws" {
 }
 
 resource "aws_db_instance" "example_database" {
-  engine            = "mysql"
-  allocated_storage = "${var.database_allocated_storage}"
-  instance_class    = "${var.database_instance_type}"
-  name              = "${var.database_name}"
-  username          = "${var.database_admin_username}"
-  password          = "${var.database_admin_password}"
+  engine              = "mysql"
+  allocated_storage   = "${var.database_allocated_storage}"
+  instance_class      = "${var.database_instance_type}"
+  name                = "${var.database_name}"
+  username            = "${var.database_admin_username}"
+  password            = "${var.database_admin_password}"
+  skip_final_snapshot = "true"
 }
