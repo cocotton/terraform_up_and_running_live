b8768583f6919eba196d5564f59c5c536f5923d5
Cocotton <julien.parent.trudeau@gmail.com>
2018-06-04 15:01:01 -0400

    Fix variables

---
+++ aws/dev/data-stores/mysql/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "data_stores_mysql" {
+  source = "../../../modules/data-stores/mysql"
+
+  database_name              = "dev-database"
+  database_admin_username    = "superadmin"
+  database_instance_type     = "t2.micro"
+  database_allocated_storage = "10"
+}
