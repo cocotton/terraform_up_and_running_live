02b1c2db9b0bd89df346b7cf7fd4e7311f0dbf52
Cocotton <julien.parent.trudeau@gmail.com>
2018-06-04 14:43:09 -0400

    Remove typo parameter

--- aws/dev/services/terraform-up-and-running/main.tf
+++ aws/dev/services/terraform-up-and-running/main.tf
@@ -11,5 +11,4 @@ module "terraform_up_and_running" {
   instance_type                = "t2.micro"
   database_remote_state_bucket = "cocotton-terraform-up-and-running-state"
   database_remote_state_key    = "dev/data-stores/mysql/terraform.tfstate"
-  skip_final_snapshot          = true
 }
