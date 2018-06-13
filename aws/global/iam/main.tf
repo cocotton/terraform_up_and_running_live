f7e10b92a3733b91d3d7bdc11e6f891d89c861c7
Cocotton <julien.parent.trudeau@gmail.com>
2018-06-12 21:49:25 -0400

    Set cloudwatch permissions based on an if else statement

--- aws/global/iam/main.tf
+++ aws/global/iam/main.tf
@@ -20,8 +20,46 @@ resource "aws_iam_policy" "ec2_read_only" {
   policy = "${data.aws_iam_policy_document.ec2_read_only.json}"
 }
 
+data "aws_iam_policy_document" "cloudwatch_read_only" {
+  statement {
+    effet     = "Allow"
+    actions   = ["cloudwatch:Describe*", "cloudwatch:Get*", "cloudwatch:List*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "cloudwatch_read_only" {
+  name   = "cloudwatch-read-only"
+  policy = "{data.aws_iam_policy_document.cloudwatch_read_only.json}"
+}
+
+data "aws_iam_policy_document" "cloudwatch_read_write" {
+  statement {
+    effet     = "Allow"
+    actions   = ["cloudwatch:*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "cloudwatch_read_write" {
+  name   = "cloudwatch-read-write"
+  policy = "{data.aws_iam_policy_document.cloudwatch_read_write.json}"
+}
+
 resource "aws_iam_user_policy_attachment" "ec2_read_only_access" {
   count      = "${length(var.iam_users)}"
   user       = "${element(aws_iam_user.example_iam_users.*.name, count.index)}"
   policy_arn = "${aws_iam_policy.ec2_read_only.arn}"
+}
+
+resource "aws_iam_user_policy_attachment" "neo_cloudwatch_read_only" {
+  count      = "${1 - var.give_neo_cloudwatch_read_write}"
+  user       = "${aws_iam_user.example_iam_users.0.name}"
+  policy_arn = "${aws_iam_policy.cloudwatch_read_only.arn}"
+}
+
+resource "aws_iam_user_policy_attachment" "neo_cloudwatch_read_write" {
+  count      = "${var.give_neo_cloudwatch_read_write}"
+  user       = "${aws_iam_user.example_iam_users.0.name}"
+  policy_arn = "${aws_iam_policy.cloudwatch_read_write.arn}"
 }
--- aws/global/iam/vars.tf
+++ aws/global/iam/vars.tf
@@ -3,3 +3,7 @@ variable "iam_users" {
   type        = "list"
   default     = ["neo", "trinity", "morpheus"]
 }
+
+variable "give_neo_cloudwatch_read_write" {
+  description = "If true. net gets full access to CloudWatch"
+}
